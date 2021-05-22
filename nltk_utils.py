aba79309bee325c4ce179edbbf73f4ae94ac1b3f
Jagveer Sangha <jagveersangha@gmail.com>
2021-05-22 17:37:27 -0400

    Added testing to ensure code works

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,3 +1,29 @@
+from nltk.stem.porter import PorterStemmer
 import nltk
+# Downloads an initial pkg to avoid tokenize errors
+# nltk.download('punkt')
+
+stemmer = PorterStemmer()
 
 
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+
+def stem(word):
+    return stemmer.stem(word.lower())
+
+
+def bag_of_words(tokenize):
+    pass
+
+
+# testing
+a = "How long does shipping take?"
+print(a)
+a = tokenize(a)
+print(a)
+
+words = ["Organize", "organizes", "organizing"]
+stemmed_words = [stem(w) for w in words]
+print(stemmed_words)
