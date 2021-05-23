0dbe449037a1d66954d4a0bb580318bf9c8e1be3
Jagveer Sangha <jagveersangha@gmail.com>
2021-05-23 17:17:44 -0400

    Added bag_of_words and testing

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,4 +1,5 @@
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 import nltk
 # Downloads an initial pkg to avoid tokenize errors
 # nltk.download('punkt')
@@ -16,17 +17,28 @@ def tokenize(sentence):
 def stem(word):
     return stemmer.stem(word.lower())
 
-
-def bag_of_words(tokenize):
-    pass
-
-
 # testing
-# a = "How long does shipping take?"
-# print(a)
-# a = tokenize(a)
-# print(a)
+    # a = "How long does shipping take?"
+    # print(a)
+    # a = tokenize(a)
+    # print(a)
 
-# words = ["Organize", "organizes", "organizing"]
-# stemmed_words = [stem(w) for w in words]
-# print(stemmed_words)
+    # words = ["Organize", "organizes", "organizing"]
+    # stemmed_words = [stem(w) for w in words]
+    # print(stemmed_words)
+
+
+def bag_of_words(tokenized_sentence, all_words):
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+
+    bag = np.zeros(len(all_words), dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx] = 1
+    return bag
+
+# Testing to ensure training works
+# sentence = ["hello", "how", "are", "you"]
+# words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+# testing = bag_of_words(sentence, words)
+# print(testing)
